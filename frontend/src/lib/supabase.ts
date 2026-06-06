21c4c8ba77687b289f1645d846a30be7e1880bbd
Caio Pardim <caiopardim@Caios-MacBook-Air.local>
2026-06-06 17:43:49 -0300

    Update Supabase anon key

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ofpyuzdqfpqienlnzjwi.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mcHl1emRxZnBxaWVubG56andpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNjM1NTAsImV4cCI6MjA2NDczOTU1MH0.MS1saFBaxmHel8BIMbIbfBPGbPtBpnZsQZo0QpT8680';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mcHl1emRxZnBxaWVubG56andpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2OTI3NzUsImV4cCI6MjA5NjI2ODc3NX0.MS1saFBaxmHel8BIMbIbfBPGbPtBpnZsQZo0QpT8680';
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
