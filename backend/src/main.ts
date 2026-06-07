4d005f56980af3e8aeee0132a331feb93249255a
Caio Pardim <caiopardim@Caios-MacBook-Air.local>
2026-06-07 13:42:01 -0300

    Set global WebSocket polyfill for Node.js 20 compatibility

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,6 @@
+import ws from 'ws';
+(global as any).WebSocket = ws;
+
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
